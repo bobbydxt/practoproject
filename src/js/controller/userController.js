3bae4c7ef360ef5e45bbc1bfc8f1722a9a1fee5f
bobbydixit-practo <bobby.dixit@practo.com>
2016-08-23 00:29:43 +0530

    Code Improvements

--- src/js/controller/userController.js
+++ src/js/controller/userController.js
@@ -7,18 +7,17 @@
 		 	//	userFactory.routeNotLoggedIn();
 		 $scope.login = function(userInfo) {
 
-		 	if($scope.form.$valid) {
-		 		//login
-		 		userFactory.login(userInfo.email,userInfo.password);
-    		} else {
-      			console.log('Error : Invalid form pushed User');
-    		}
+		 	callHandler(userInfo,userFactory.login);
 		 }
 		 $scope.signUp = function(userInfo) {
 
+		 	callHandler(userInfo,userFactory.signup);
+		 }
+		 function callHandler(userInfo,caller)
+		 {
 		 	if($scope.form.$valid) {	
 		 		//signup
-		 		userFactory.signup(userInfo.email,userInfo.password);
+		 		caller(userInfo.email,userInfo.password);
     		} else {
       			console.log('Error : Invalid form pushed User');
     		}
