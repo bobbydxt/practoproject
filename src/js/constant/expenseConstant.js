d2fc9f3a976cb90aa06fd6c8502ee89c2bdc5220
bobbydixit-practo <bobby.dixit@practo.com>
2016-08-24 00:32:53 +0530

    minor improvements

--- src/js/constant/expenseConstant.js
+++ src/js/constant/expenseConstant.js
@@ -6,7 +6,7 @@
 			'data': {
 				'1': {'name': 'Salary',
 					'id': 1,
-					subCatagory:{
+					'subCatagory':{
 						'1': {
 							'name':'Primary Income',
 							'id': 1
@@ -26,113 +26,113 @@
 					}},
 				'2': {'name': 'Awards',
 					'id': 2,
-					subCatagory:{
+					'subCatagory':{
 						'1': {
 							'name':'Organizational',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Competitions',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Events',
 							'id': 3
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				3: {'name': 'Gifts',
+				'3': {'name': 'Gifts',
 					'id': 3,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Friends and Family',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Contests',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Events',
 							'id': 3
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				4: {'name': 'Sellings',
+				'4': {'name': 'Sellings',
 					'id': 4,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Property',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Goods',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Services',
 							'id': 3
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				5: {'name': 'Interests',
+				'5': {'name': 'Interests',
 					'id': 5,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Investments',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Deposits',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Informal Lending',
 							'id': 3
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
 
-				6: {'name': 'Rental',
+				'6': {'name': 'Rental',
 					'id': 6,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Property',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Goods',
 							'id': 2
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				7: {'name': 'Tax Return',
+				'7': {'name': 'Tax Return',
 					'id': 7,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Due to Excess payment',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Due to rebates',
 							'id': 2
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
@@ -143,236 +143,236 @@
 					'id': 2,
 					'name': 'debit',
 					'data': {
-				1: {'name': 'Food and Beverage',
+				'1': {'name': 'Food and Beverage',
 					'id': 1,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Online order',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Restaurants',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Cafe',
 							'id': 3
 						},
-						4: {
+						'4': {
 							'name':'Canteen',
 							'id': 4
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
 
-				2: {'name': 'Bills and Utility',
+				'2': {'name': 'Bills and Utility',
 					'id': 2,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Phone',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Water',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Electricity',
 							'id': 3
 						},
-						4: {
+						'4': {
 							'name':'Gas',
 							'id': 4
 						},
-						5: {
+						'5': {
 							'name':'Internet',
 							'id': 5
 						},
-						6: {
+						'6': {
 							'name':'Tv subscriptions',
 							'id': 6
 						},
-						7: {
+						'7': {
 							'name':'Rentals',
 							'id': 7
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				3: {'name': 'Transportation',
+				'3': {'name': 'Transportation',
 					'id': 3,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Cabs',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Parking',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Petrol',
 							'id': 3
 						},
-						4: {
+						'4': {
 							'name':'Maintenance',
 							'id': 4
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
 
-				4: {'name': 'Shopping',
+				'4': {'name': 'Shopping',
 					'id': 4,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Cloaths',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Footware',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Accessories',
 							'id': 3
 						},
-						4: {
+						'4': {
 							'name':'Electronics',
 							'id': 4
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				5: {'name': 'Entertainment',
+				'5': {'name': 'Entertainment',
 					'id': 5,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Movie',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Games',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Events',
 							'id': 3
 						},
-						4: {
+						'4': {
 							'name':'Sports',
 							'id': 4
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				6: {'name': 'Health and Fitness',
+				'6': {'name': 'Health and Fitness',
 					'id': 6,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Doctor',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Pharmacy',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Personal Care',
 							'id': 3
 						},
-						4: {
+						'4': {
 							'name':'Diagnostic',
 							'id': 4
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				7: {'name': 'Gifts and Donations',
+				'7': {'name': 'Gifts and Donations',
 					'id': 7,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Charity',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Informal',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Marriage',
 							'id': 3
 						},
-						4: {
+						'4': {
 							'name':'Funeral',
 							'id': 4
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				8: {'name': 'Education',
+				'8': {'name': 'Education',
 					'id': 8,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Books',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Fees',
 							'id': 2
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				9: {'name': 'Withdrawal',
+				'9': {'name': 'Withdrawal',
 					'id': 9,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Check',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Cash',
 							'id': 2
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
 					}},
-				10: {'name': 'Investments',
+				'10': {'name': 'Investments',
 					'id': 10,
-					subCatagory:{
-						1: {
+					'subCatagory':{
+						'1': {
 							'name':'Insurance',
 							'id': 1
 						},
-						2: {
+						'2': {
 							'name':'Mutual Fund',
 							'id': 2
 						},
-						3: {
+						'3': {
 							'name':'Equity',
 							'id': 3
 						},
-						0: {
+						'0': {
 							'name': 'Other',
 							'id': 0
 						}
