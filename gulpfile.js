e7d69c21d50f06c26d79e42a5aad62428e0eb9cf
bobbydixit-practo <bobby.dixit@practo.com>
2016-08-26 02:32:28 +0530

    login signup css added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp=require('gulp');
 var sass=require('gulp-sass');
 gulp.task('sass',function(){
-	return gulp.src('./src/sass/*.scss')
+	return gulp.src('./src/sass/**/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('./src/css'));
 });
